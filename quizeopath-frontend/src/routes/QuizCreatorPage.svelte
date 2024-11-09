431f941d1202ab45860d70f615168a75bd45a953
WilliamFitzjohn <william_fitzjohn@icloud.com>
2024-11-09 12:15:07 -0500

    author field

--- quizeopath-frontend/src/routes/QuizCreatorPage.svelte
+++ quizeopath-frontend/src/routes/QuizCreatorPage.svelte
@@ -11,13 +11,13 @@
   
   async function createQuiz() {
     try {
-      quiz.author = user.id;
       const { 
         updated: emptyUpdated,
         collectionId: emptyCollectionId,
         collectionName: emptyCollectionName,
         ...quizWithoutIdAndUpdated 
       } = quiz;
+      quiz.author = $user?.id || '';
       const createdQuiz = await pb.collection('quizzes').create(quizWithoutIdAndUpdated);
       console.log('Created quiz:', createdQuiz);
       id = createdQuiz.id
